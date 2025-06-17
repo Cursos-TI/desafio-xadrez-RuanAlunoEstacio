93dbd199a057b43258b339c2902637c5108d5789
RuanAlunoEstacio <psoares3568@gmail.com>
2025-06-17 03:02:41 +0000

    Ajustes e reagrupamento do código para melhor lei- tura

--- xadrez.c
+++ xadrez.c
@@ -7,21 +7,21 @@
 void moveBishop(int square){
     if(square > 0){
         moveBishop(square-1);
-        printf("Cima Direita\n");
+        printf("- Cima Direita\n");
     }
 }
 
 void moveRook(int square){
     if(square > 0){
         moveRook(square-1);
-        printf("Direita\n");
+        printf("- Direita\n");
     }
 }
 
 void moveQueen(int square){
     if(square > 0){
         moveQueen(square-1);
-        printf("Esquerda\n");
+        printf("- Esquerda\n");
     }
 }
 
@@ -37,30 +37,24 @@ int main() {
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-    printf("Movimentação do Bispo com recursividade:\n");
-    moveBishop(bishop);
     printf("\nMovimentação do Bispo sem recursividade:\n");
     for(int i = 0; i < bishop; i++){
-        printf("Cima Direita\n");
+        printf("- Cima Direita\n");
     }
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-    printf("\nMovimentação da Torre com recursividade:\n");
-    moveRook(rook);
     printf("\nMovimentação da Torre sem recursividade:\n");
     while(rookMoves < rook) {
-        printf("Direita\n");
+        printf("- Direita\n");
         rookMoves++;
     }
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-    printf("\nMovimentação da Rainha com recursividade:\n");
-    moveQueen(queen);
     printf("\nMovimentação da Rainha sem recursividade:\n");
     do{
-        printf("Esquerda\n");
+        printf("- Esquerda\n");
         queenMoves++;
     } while(queenMoves < queen);
 
@@ -69,22 +63,9 @@ int main() {
     // Um loop pode representar a movimentação horizontal e outro vertical.
     printf("\nMovimentação do Cavalo nível aventureiro: ");
     for(int i = 1; i < horse; i++){
-        printf("\nBaixo");
+        printf("\n- Baixo");
         while(i==horse-1){
-            printf("\nEsquerda\n");
-            break;
-        }
-    }
-
-    //Movimentação do cavalo nível Master
-    //Não precisaria de mais de uma variável para fazer isso mas é pedido para usar variável múltipla ou condição múltipla
-    printf("\nMovimentação do Cavalo nível Master: ");
-    for(int i = 1, j = 0; i <= horse && j < horse; i++, j++){
-        if(i < horse){
-            printf("\nCima");
-        }
-        if(j == horse-1){
-            printf("\nDireita\n");
+            printf("\n- Esquerda\n");
             break;
         }
     }
@@ -93,6 +74,30 @@ int main() {
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 
+    //Movimentação do Bispo nível Master
+    printf("\nMovimentação do Bispo com recursividade:\n");
+    moveBishop(bishop);
+
+    //Movimentação da Torre nível Master
+    printf("\nMovimentação da Torre com recursividade:\n");
+    moveRook(rook);
+
+    //Movimentação da Rainha nível Master
+    printf("\nMovimentação da Rainha com recursividade:\n");
+    moveQueen(queen);
+
+    //Movimentação do cavalo nível Master
+    //Não precisaria de mais de uma variável para fazer isso mas é pedido para usar variável múltipla ou condição múltipla
+    printf("\nMovimentação do Cavalo nível Master: ");
+    for(int i = 1, j = 0; i <= horse && j < horse; i++, j++){
+        if(i < horse){
+            printf("\n- Cima");
+        }
+        if(j == horse-1){
+            printf("\n- Direita\n");
+            break;
+        }
+    }
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
